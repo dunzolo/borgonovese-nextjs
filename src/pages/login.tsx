c403ef3e53b775947b555e6ba04eac5cc4dc3319
Davide Rossi <rossidavide108@gmail.com>
2024-04-08 23:15:26 +0200

    🐛 layout login and size adjust

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-[100vh] flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -88,23 +88,6 @@ export default function LoginPage() {
               />
               <Button type="submit">Log in</Button>
             </form>
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
           </div>
         </div>
       </div>
